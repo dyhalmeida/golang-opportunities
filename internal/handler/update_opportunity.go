eae92b353b48bc7bc53930de79eef01c7811bad4
Diego Almeida <dyhalmeida@gmail.com>
2023-07-25 20:42:21 -0300

    feat: add update opening handler

--- internal/handler/update_opportunity.go
+++ internal/handler/update_opportunity.go
@@ -3,15 +3,56 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dyhalmeida/golang-opportunities/internal/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpportunityHandler(ctx *gin.Context) {
-
+	request := UpdateOpeningRequest{}
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
 	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "routerParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening with id: "+id+" not found")
+		return
+	}
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT opening by id: " + id,
-	})
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
 
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	sendSuccess(ctx, "update-opening", opening)
 }
