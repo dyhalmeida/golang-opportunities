f182ce5ebfc37933f939fdafc34f794a0cb1aef8
Diego Almeida <dyhalmeida@gmail.com>
2023-07-25 16:21:44 -0300

    feat: add list openings handler

--- internal/handler/list_opportunity.go
+++ internal/handler/list_opportunity.go
@@ -3,13 +3,15 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dyhalmeida/golang-opportunities/internal/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpportunityHandler(ctx *gin.Context) {
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
-
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
