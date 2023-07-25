fdc3d13c216a7cf85e1aaa1a7d79c6a48bdf554f
Diego Almeida <dyhalmeida@gmail.com>
2023-07-25 16:28:22 -0300

    feat: add show opening handler

--- internal/handler/show_opportunity.go
+++ internal/handler/show_opportunity.go
@@ -3,15 +3,20 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dyhalmeida/golang-opportunities/internal/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpportunityHandler(ctx *gin.Context) {
-
 	id := ctx.Param("id")
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening by id: " + id,
-	})
-
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "routerParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening with id: "+id+" not found")
+		return
+	}
+	sendSuccess(ctx, "show-opening", opening)
 }
