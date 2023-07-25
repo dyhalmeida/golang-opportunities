bb3ae62c70d11f91f79334a7637876f3364383a7
Diego Almeida <dyhalmeida@gmail.com>
2023-07-25 16:14:14 -0300

    feat: add delete opening handler

--- internal/handler/delete_opportunity.go
+++ internal/handler/delete_opportunity.go
@@ -3,15 +3,23 @@ package handler
 import (
 	"net/http"
 
+	"github.com/dyhalmeida/golang-opportunities/internal/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpportunityHandler(ctx *gin.Context) {
-
 	id := ctx.Param("id")
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE opening by id: " + id,
-	})
-
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "routerParameter").Error())
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening with id: "+id+" not found")
+		return
+	}
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error deleting opening with id: "+id)
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
 }
